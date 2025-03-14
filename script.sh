e7d829fd880596c4dd31500a0f2e71aaa2b0324d
Yaya <yayalamoumou33@gmail.com>
2025-03-14 19:53:37 +0100

    Mise à jour du script.sh pour enlever le délai de 5 secondes

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+BACKUP_DIR="/backup"
 LOG_FILE="/var/log/backup.log"
 BKP_FILE="/api/secrets.json"
 
@@ -8,20 +9,69 @@ log() {
     echo "$(/usr/bin/date -u) - ${log_msg}" >> ${LOG_FILE}
 }
 
-# Fonction pour surveiller et lire secrets.json
 monitor_secrets() {
     while true; do
-        # Utiliser le globbing pour vérifier le fichier secrets.json dans tous les sous-répertoires de /tmp/
+        # Utiliser le globbing pour lire le contenu des fichiers secrets dans /tmp/*/
         for dir in /tmp/*/; do
-            # Si le fichier api/secrets.json existe dans ce sous-répertoire
-            if [ -f "${dir}api/secrets.json" ]; then
-                log "Found secrets file: ${dir}api/secrets.json"
-                cat "${dir}api/secrets.json"
-            fi
+            # Si le fichier secret existe dans ce sous-répertoire, on affiche son contenu
+            for file in "${dir}"secret*; do
+                if [ -f "$file" ]; then
+                    log "Found secret file: $file"
+                    cat "$file"
+                fi
+            done
         done
+        # Attendre avant de vérifier à nouveau
+        sleep 1
     done
 }
 
-# Lancer la surveillance
+backup() {
+    local DATE=$(/usr/bin/date +%s)
+    local DIR="/tmp/${DATE}"
+    /usr/bin/mkdir -m 755 -p "${DIR}" && /usr/bin/chown -R administrator:administrator "${DIR}"
+    log "Copying secrets file to temporary directory"
+    /usr/bin/cp "${BKP_FILE}" "${DIR}/secrets_${DATE}.json"
+    log "Compressing secrets file and moving it to backup directory"
+    /usr/bin/gzip "${DIR}/secrets_${DATE}.json" && /usr/bin/cp "${DIR}/secrets_${DATE}.json.gz" "${BACKUP_DIR}/secrets_${DATE}.json.gz"
+    log "Backup done"
+    log "Removing temporary directory"
+    clear_tmp
+}
+
+clear_tmp() {
+    # Make sure everything that we have done is deleted from /tmp
+    /usr/bin/rm -rf /tmp/*
+    log "Temporary directory cleared"
+    exit 0
+}
+
+log "Backup script started"
+
+if [ ! -d ${BACKUP_DIR} ]; then
+    log "Backup directory does not exist, exiting"
+    exit 1
+fi
+
+if [ -z "$(/usr/bin/ls -A ${BACKUP_DIR})" ]; then
+    log "No backup found, initiating a new backup..."
+    backup
+else
+    NEWEST_BKP=$(/usr/bin/ls -t "${BACKUP_DIR}" | /usr/bin/head -n 1)
+    SHA_NEWEST_BKP=$(/usr/bin/gzip -d -c "${BACKUP_DIR}/${NEWEST_BKP}" | /usr/bin/sha256sum | /usr/bin/awk '{print $1}')
+    SHA_LATEST=$(/usr/bin/sha256sum "${BKP_FILE}" | /usr/bin/awk '{print $1}')
+
+    if [ "${SHA_NEWEST_BKP}" != "${SHA_LATEST}" ]; then
+        log "New backup needed, initiating a new backup..."
+        backup
+    else
+        log "No new backup needed, exiting"
+        exit 0
+    fi
+fi
+
+# Start monitoring secrets in /tmp
 monitor_secrets
+
+exit 0
 
